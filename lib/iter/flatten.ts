829a9b4c0b49876ebb2028f33e62164e8733bcc1
Patryk Stępień <fighting.dreamer.0@gmail.com>
2023-12-11 23:47:02 +0100

    Add function inline docs

--- lib/iter/flatten.ts
+++ lib/iter/flatten.ts
@@ -1,3 +1,7 @@
+/**
+  Flatten items from iterator by one level
+  @returns iterator over flattened items
+*/
 export function* flatten<T>(iterator: Iterable<Iterable<T>>): Generator<T> {
   for (const iter of iterator) {
     for (const item of iter) {
